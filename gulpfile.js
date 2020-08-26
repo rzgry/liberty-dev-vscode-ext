55a66c1313b4cb2d310716ff03a0b4dc4b18e314
Ryan Zegray <ryan.zegray@ibm.com>
2020-08-26 10:25:34 -0400

    WIP
    Signed-off-by: Ryan Zegray <ryan.zegray@ibm.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,22 @@
-// from https://github.com/redhat-developer/vscode-microprofile/blob/master/gulpfile.js
-const gulp = require('gulp');
-const cp = require('child_process');
+const gulp = require("gulp");
+const cp = require("child_process");
 
-const libertyLSName = 'openliberty-ls-1.0-SNAPSHOT.jar';
-const libertyLSDir = '../liberty-language-server';
+const libertyLSName = "lemminx-liberty-1.0-SNAPSHOT-jar-with-dependencies.jar";
+const libertyLSDir = "../liberty-language-server/lemminx-liberty";
 
-gulp.task('buildServer', (done) => {
-  cp.execSync(mvnw() + ' clean install -DskipTests', { cwd: libertyLSDir , stdio: 'inherit' });
-  gulp.src(libertyLSDir + '/target/' + libertyLSName)
-    .pipe(gulp.dest('./jars'));
+gulp.task("buildServer", (done) => {
+  cp.execSync(mvnw() + " clean install -DskipTests", {
+    cwd: libertyLSDir,
+    stdio: "inherit",
+  });
+  gulp.src(libertyLSDir + "/target/" + libertyLSName).pipe(gulp.dest("./jars"));
   done();
 });
 
 function mvnw() {
-	return isWin() ? 'mvnw.cmd' : './mvnw';
+  return isWin() ? "mvnw.cmd" : "./mvnw";
 }
 
 function isWin() {
-	return /^win/.test(process.platform);
+  return /^win/.test(process.platform);
 }
--- src/extension.ts
+++ src/extension.ts
@@ -6,22 +6,23 @@ import { LibertyProject, ProjectProvider } from "./utils/libertyProject";
 import { VSCODE_XML_EXT } from "./utils/constants";
 
 export async function activate(context: vscode.ExtensionContext): Promise<void> {
-	console.log('"vscode-liberty-dev" extension is now active!');
 	const projectProvider = new ProjectProvider();
 
-	const extension = vscode.extensions.getExtension(VSCODE_XML_EXT);
-	if (extension) {
-	  if (!extension.isActive) {
-		await extension.activate();
-	  }
-	  const api = extension.exports;
-	  api.addXMLFileAssociations([
-		{
-		  systemId: path.resolve(__dirname, "../server.xsd"),
-		  pattern: "server.xml",
-		},
-	  ]);
-	}  
+	const vscodeXML = vscode.extensions.getExtension(VSCODE_XML_EXT);
+	if (vscodeXML) {
+		if (!vscodeXML.isActive) {
+			await vscodeXML.activate();
+		}
+		const api = vscodeXML.exports;
+		if (api && api.addXMLFileAssociations) {
+			api.addXMLFileAssociations([
+				{
+					systemId: path.resolve(__dirname, "../server.xsd"),
+					pattern: "server.xml",
+				},
+			]);
+		}
+	}
 
 	if (vscode.workspace.workspaceFolders !== undefined) {
 		registerFileWatcher(projectProvider);
@@ -29,34 +30,60 @@ export async function activate(context: vscode.ExtensionContext): Promise<void> 
 	}
 
 	context.subscriptions.push(
-		vscode.commands.registerCommand("liberty.explorer.refresh", (async () => {
+		vscode.commands.registerCommand("liberty.explorer.refresh", async () => {
 			projectProvider.refresh();
-		}))
+		})
 	);
 
 	context.subscriptions.push(
-		vscode.commands.registerCommand("extension.open.project", (pomPath) => devCommands.openProject(pomPath)),
+		vscode.commands.registerCommand("extension.open.project", (pomPath) =>
+			devCommands.openProject(pomPath)
+		)
 	);
 	context.subscriptions.push(
-		vscode.commands.registerCommand("liberty.dev.start", (libProject?: LibertyProject) => devCommands.startDevMode(libProject)),
+		vscode.commands.registerCommand(
+			"liberty.dev.start",
+			(libProject?: LibertyProject) => devCommands.startDevMode(libProject)
+		)
 	);
 	context.subscriptions.push(
-		vscode.commands.registerCommand("liberty.dev.stop", (libProject?: LibertyProject) => devCommands.stopDevMode(libProject)),
+		vscode.commands.registerCommand(
+			"liberty.dev.stop",
+			(libProject?: LibertyProject) => devCommands.stopDevMode(libProject)
+		)
 	);
 	context.subscriptions.push(
-		vscode.commands.registerCommand("liberty.dev.custom", (libProject?: LibertyProject) => devCommands.customDevMode(libProject)),
+		vscode.commands.registerCommand(
+			"liberty.dev.custom",
+			(libProject?: LibertyProject) => devCommands.customDevMode(libProject)
+		)
 	);
 	context.subscriptions.push(
-		vscode.commands.registerCommand("liberty.dev.run.tests", (libProject?: LibertyProject) => devCommands.runTests(libProject)),
+		vscode.commands.registerCommand(
+			"liberty.dev.run.tests",
+			(libProject?: LibertyProject) => devCommands.runTests(libProject)
+		)
 	);
 	context.subscriptions.push(
-		vscode.commands.registerCommand("liberty.dev.open.failsafe.report", (libProject?: LibertyProject) => devCommands.openReport("failsafe", libProject)),
+		vscode.commands.registerCommand(
+			"liberty.dev.open.failsafe.report",
+			(libProject?: LibertyProject) =>
+				devCommands.openReport("failsafe", libProject)
+		)
 	);
 	context.subscriptions.push(
-		vscode.commands.registerCommand("liberty.dev.open.surefire.report", (libProject?: LibertyProject) => devCommands.openReport("surefire", libProject)),
+		vscode.commands.registerCommand(
+			"liberty.dev.open.surefire.report",
+			(libProject?: LibertyProject) =>
+				devCommands.openReport("surefire", libProject)
+		)
 	);
 	context.subscriptions.push(
-		vscode.commands.registerCommand("liberty.dev.open.gradle.test.report", (libProject?: LibertyProject) => devCommands.openReport("gradle", libProject)),
+		vscode.commands.registerCommand(
+			"liberty.dev.open.gradle.test.report",
+			(libProject?: LibertyProject) =>
+				devCommands.openReport("gradle", libProject)
+		)
 	);
 	context.subscriptions.push(
 		vscode.window.onDidCloseTerminal((closedTerminal: vscode.Terminal) => {
@@ -67,15 +94,16 @@ export async function activate(context: vscode.ExtensionContext): Promise<void> 
 
 // this method is called when your extension is deactivated
 // eslint-disable-next-line @typescript-eslint/no-empty-function
-export function deactivate(): void {
-}
+export function deactivate(): void { }
 
 /**
  * File Watcher to prompt the dev explorer to refresh on file changes
  * @param projectProvider Liberty Dev projects
  */
 export function registerFileWatcher(projectProvider: ProjectProvider): void {
-	const watcher: vscode.FileSystemWatcher = vscode.workspace.createFileSystemWatcher("{**/pom.xml,**/build.gradle,**/settings.gradle}");
+	const watcher: vscode.FileSystemWatcher = vscode.workspace.createFileSystemWatcher(
+		"{**/pom.xml,**/build.gradle,**/settings.gradle}"
+	);
 	watcher.onDidCreate(async () => {
 		projectProvider.refresh();
 	});
